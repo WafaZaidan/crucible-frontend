21f97cf73db5c4ea6d6f17f9d3f2929c26e9333b
nuurcodes <71561866+nuurcodes@users.noreply.github.com>
2021-05-05 15:53:10 +0100

    Add modal styling (#65)

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -9,12 +9,90 @@ export const GlobalStyles = css`
     outline: none;
     box-shadow: none;
   }
-  .bn-onboard-modal {
-    z-index: 999;
-  }
-  .bn-notify-notification-info-message a {
-    text-decoration: underline;
-    font-weight: bold;
-    color: #4299e1;
-  }
+  .bn-onboard-custom.bn-onboard-modal {
+    z-index: 9999;
+    padding: 0 1.5rem;
+    font-family: Poppins;
+
+    .bn-onboard-modal-content.bn-onboard-dark-mode {
+      background: #180535;
+      width: 100%;
+      max-width: 480px;
+      border-radius: 2rem;
+      padding-right: 0.6rem;
+
+      .bn-onboard-modal-content-close {
+        svg {
+          height: 14px;
+          width: 14px;
+        }
+        :hover {
+          background: unset;
+        }
+      }
+
+      .bn-onboard-modal-select-wallets {
+        flex-flow: unset;
+        flex-direction: column;
+        overflow: auto;
+        height: 400px;
+        margin-top: 1.25rem;
+        margin-bottom: 0.5rem;
+
+        & > li {
+          width: 100%;
+          padding-right: 0.8em;
+
+          & > button {
+            height: 70px;
+            width: inherit;
+            justify-content: space-between;
+            flex-direction: row-reverse;
+            border-radius: 1rem;
+            border: 1px solid #304370;
+
+            :hover {
+              border-color: #5CE3FF;
+              background: inherit;
+            }
+
+            &.bn-onboard-selected-wallet {
+              background: inherit;
+              border-color: inherit;
+
+              :hover {
+                border-color: #5CE3FF;
+              }
+            }
+
+            & > span {
+              font-size: 1.3rem;
+              font-weight: 500;
+              margin-left: 0;
+
+              & > i {
+                display: none;
+              }
+            }
+
+            & > div {
+              height: 30px;
+              width: 30px;
+            }
+          }
+        }
+      }
+
+      .bn-onboard-modal-content-header,
+      .bn-onboard-select-description,
+      .bn-onboard-select-wallet-info,
+      .bn-onboard-prepare-button {
+        display: none;
+      }
+    }
+    .bn-notify-notification-info-message a {
+      text-decoration: underline;
+      font-weight: bold;
+      color: #4299e1;
+    }
 `;
