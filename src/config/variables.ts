d3f8dcd6b982e0a7feb472b434effbdf28c3e219
nuurcodes <71561866+nuurcodes@users.noreply.github.com>
2021-05-12 11:11:49 +0100

    Update links (#92)

--- src/config/variables.ts
+++ src/config/variables.ts
@@ -21,10 +21,8 @@ export const config =
         dappId: 'ad454b00-3218-4403-95e9-22c3c7d3adc0',
         uniswapSubgraphUrl:
           'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2',
-        uniswapPoolUrl:
-          'https://app.uniswap.org/#/add/v2/0x88ACDd2a6425c3FaAE4Bc9650Fd7E27e0Bebb7aB/ETH',
-        getMistUrl:
-          'https://app.uniswap.org/#/swap?outputCurrency=0x88acdd2a6425c3faae4bc9650fd7e27e0bebb7ab&use=V2', // V2
+        uniswapPoolUrl: 'http://lp.mist.alchemist.wtf',
+        getMistUrl: 'http://swap.mist.alchemist.wtf', // V2
         // 'https://app.uniswap.org/#/swap?outputCurrency=0x88acdd2a6425c3faae4bc9650fd7e27e0bebb7ab', // will point to v3,
         mintCrucibleDoc:
           'https://docs.alchemist.wtf/mist/crucible/guides-crucible.alchemist.wtf/how-do-i-mint-a-crucible',
