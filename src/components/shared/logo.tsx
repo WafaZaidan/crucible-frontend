a1a89c3fb8c8680a1878038d1825a69c36bd099c
nuurcodes <nuurcodes@gmail.com>
2021-05-15 01:45:40 +0100

    CRU-19

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -6,13 +6,24 @@ import logo from '../../img/logo.png';
 
 type Props = {
   widthList?: number[];
+  heightList?: number[];
 };
 
-const Logo: FC<Props> = ({ widthList = ['140px', '140px', '200px'] }) => {
+const Logo: FC<Props> = ({
+  widthList = ['30px', '140px', '200px'],
+  heightList = ['30px', 'auto', 'auto'],
+}) => {
   return (
     <LinkBox>
       <LinkOverlay as={Link} to='/'>
-        <Image src={logo} width={widthList} alt='alchemist logo' />
+        <Image
+          src={logo}
+          width={widthList}
+          height={heightList}
+          objectFit='cover'
+          objectPosition='0% 0'
+          alt='alchemist logo'
+        />
       </LinkOverlay>
     </LinkBox>
   );
