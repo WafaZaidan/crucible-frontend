9c564298f8125897d593fc3a8c1d0de6728a2b0f
nuurcodes <nuurcodes@gmail.com>
2021-05-15 19:04:23 +0100

    Update links

--- src/config.ts
+++ src/config.ts
@@ -8,10 +8,6 @@ const config = {
   inflationStartTimestamp: 1612661126000,
   uniswapSubgraphUrl:
     'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2',
-  uniswapPoolUrl:
-    'https://app.uniswap.org/#/add/v2/0xF6c1210Aca158bBD453A12604A03AeD2659ac0ef/ETH',
-  getMistUrl:
-    'https://app.uniswap.org/#/swap?outputCurrency=0xF6c1210Aca158bBD453A12604A03AeD2659ac0ef&use=V2',
   mintCrucibleDoc:
     'https://docs.alchemist.wtf/mist/crucible/guides-crucible.alchemist.wtf/how-do-i-mint-a-crucible',
   lpTokensDoc:
