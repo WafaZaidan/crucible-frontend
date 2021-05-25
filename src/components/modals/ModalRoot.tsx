e6f7bef1bb86ed3aaa7a32650198870852cb750e
NinjaNuur <71561866+nuurcodes@users.noreply.github.com>
2021-05-25 21:23:59 +0100

    hotfix: unsubscribe modal bug (#110)

--- src/components/modals/ModalRoot.tsx
+++ src/components/modals/ModalRoot.tsx
@@ -3,15 +3,18 @@ import { useModal } from '../../store/modals';
 import { ModalType } from './types';
 import ConnectWalletModal from './ConnectWalletModal';
 import WalletInfoModal from './WalletInfoModal';
+import UnstakeAndClaimModal from './unstakeAndClaimModal';
 
 const ModalRoot: FC = () => {
-  const { modalType } = useModal();
+  const { modalType, modalProps } = useModal();
 
   switch (modalType) {
     case ModalType.connectWallet:
       return <ConnectWalletModal />;
     case ModalType.walletInfo:
       return <WalletInfoModal />;
+    case ModalType.claimRewards:
+      return <UnstakeAndClaimModal {...modalProps} />;
   }
 
   return null;
