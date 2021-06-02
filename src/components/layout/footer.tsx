06fd9ff894714586a5f087d143ccd80cd812e52b
NinjaNuur <71561866+nuurcodes@users.noreply.github.com>
2021-06-02 21:55:31 +0100

    Fix logo issue mobile (#111)

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,10 +2,25 @@ import React, { FC } from 'react';
 import { IconButton } from '@chakra-ui/button';
 import { externalLinks } from '../../config/links';
 import { Flex, HStack, Link as ChakraLink, Box } from '@chakra-ui/layout';
+import { __PROD__ } from '../../utils/constants';
+import { Switch } from '@chakra-ui/switch';
+import { useFeatureFlag } from '../../store/featureFlag';
+import { FormControl, FormLabel } from '@chakra-ui/form-control';
 
 const Footer: FC = () => {
+  const { toggleFlag } = useFeatureFlag();
   return (
     <Flex p={4} alignItems='flex-end' flexDirection='column'>
+      {!__PROD__ && (
+        <Box position='absolute' bottom={8} left={8}>
+          <FormControl display='flex' alignItems='center'>
+            <FormLabel mb='0'>1.3.0-b.3</FormLabel>
+            <Switch
+              onChange={() => toggleFlag('enableMultipleRewardPrograms')}
+            />
+          </FormControl>
+        </Box>
+      )}
       <Box>
         <HStack spacing={4}>
           {externalLinks.map(({ href, label, icon: Icon }) => (
